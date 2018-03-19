5a5c559b02386b3b8fa85d8295336b7b96211465
AzureAD\박정훈 <pkekzm94@naver.com>
2018-03-19 18:52:31 +0900

    .

--- Client/Framework Project/Framework Project/Camera.h
+++ Client/Framework Project/Framework Project/Camera.h
@@ -1,179 +1,84 @@
-#pragma once
-#include <DirectXMath.h>
+//***************************************************************************************
+// Camera.h by Frank Luna (C) 2011 All Rights Reserved.
+//   
+// Simple first person style camera class that lets the viewer explore the 3D scene.
+//   -It keeps track of the camera coordinate system relative to the world space
+//    so that the view matrix can be constructed.  
+//   -It keeps track of the viewing frustum of the camera so that the projection
+//    matrix can be obtained.
+//***************************************************************************************
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
 #include "Utility.h"
-#include "Framework.h"
-using namespace DirectX;
-typedef unsigned long DWORD;
-#define ASPECT_RATIO (float(FRAME_BUFFER_WIDTH) / float(FRAME_BUFFER_HEIGHT))
-#define FIRST_PERSON_CAMERA 0x01
-#define SPACESHIP_CAMERA 0x02 
-#define THIRD_PERSON_CAMERA 0x03
-#define ASPECT_RATIO (float(FRAME_BUFFER_WIDTH) / float(FRAME_BUFFER_HEIGHT))
-
-class CPlayer;
-
-struct VS_CB_CAMERA_INFO
-
-{
-
-	XMFLOAT4X4 m_xmf4x4View;
-
-	XMFLOAT4X4 m_xmf4x4Projection;
-
-};
-
-
-
-
-
+#include "MathHelper.h"
 class Camera
-
 {
-
-protected:
-	XMFLOAT3		 m_xmf3Position;
-	XMFLOAT3		 m_xmf3Right;
-	XMFLOAT3		m_xmf3Up;
-	XMFLOAT3		m_xmf3Look;
-	float           m_fPitch;
-	float           m_fRoll;
-	float           m_fYaw;
-	DWORD m_nMode;
-	XMFLOAT3		 m_xmf3LookAtWorld;
-	XMFLOAT3		m_xmf3Offset;
-	float           m_fTimeLag;
-	XMFLOAT4X4		m_xmf4x4View;
-	XMFLOAT4X4		m_xmf4x4Projection;
-	D3D12_VIEWPORT	 m_d3dViewport;
-	D3D12_RECT		 m_d3dScissorRect;
-	CPlayer *m_pPlayer = NULL;
-
 public:
+
 	Camera();
-	Camera(Camera *pCamera);
-	virtual ~Camera();
-	virtual void CreateShaderVariables(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
-	virtual void ReleaseShaderVariables(); virtual void UpdateShaderVariables(ID3D12GraphicsCommandList *pd3dCommandList);
-	void GenerateViewMatrix();
-	void GenerateViewMatrix(XMFLOAT3 xmf3Position, XMFLOAT3 xmf3LookAt, XMFLOAT3 xmf3Up);
-	void RegenerateViewMatrix();
-	void GenerateProjectionMatrix(float fNearPlaneDistance, float fFarPlaneDistance, float fAspectRatio, float fFOVAngle);
-	void SetViewport(int xTopLeft, int yTopLeft, int nWidth, int nHeight, float fMinZ = 0.0f, float fMaxZ = 1.0f);
-	void SetScissorRect(LONG xLeft, LONG yTop, LONG xRight, LONG yBottom);
-	virtual void SetViewportsAndScissorRects(ID3D12GraphicsCommandList *pd3dCommandList);
-	void SetPlayer(CPlayer *pPlayer) { m_pPlayer = pPlayer; }
+	~Camera();
 
-	CPlayer *GetPlayer() { return(m_pPlayer); }
-	void SetMode(DWORD nMode) { m_nMode = nMode; }
-	DWORD GetMode() { return(m_nMode); }
-	void SetPosition(XMFLOAT3 xmf3Position) { m_xmf3Position = xmf3Position; }
-	XMFLOAT3& GetPosition() { return(m_xmf3Position); }
-	void SetLookAtPosition(XMFLOAT3 xmf3LookAtWorld) { m_xmf3LookAtWorld = xmf3LookAtWorld; }
+	XMVECTOR GetPosition()const;
+	XMFLOAT3 GetPosition3f()const;
+	void SetPosition(float x, float y, float z);
+	void SetPosition(const XMFLOAT3& v);
 
-	XMFLOAT3& GetLookAtPosition() { return(m_xmf3LookAtWorld); }
+	XMVECTOR GetRight()const;
+	XMFLOAT3 GetRight3f()const;
+	XMVECTOR GetUp()const;
+	XMFLOAT3 GetUp3f()const;
+	XMVECTOR GetLook()const;
+	XMFLOAT3 GetLook3f()const;
 
-	XMFLOAT3& GetRightVector() { return(m_xmf3Right); }
+	float GetNearZ()const;
+	float GetFarZ()const;
+	float GetAspect()const;
+	float GetFovY()const;
+	float GetFovX()const;
 
-	XMFLOAT3& GetUpVector() { return(m_xmf3Up); }
+	float GetNearWindowWidth()const;
+	float GetNearWindowHeight()const;
+	float GetFarWindowWidth()const;
+	float GetFarWindowHeight()const;
 
-	XMFLOAT3& GetLookVector() { return(m_xmf3Look); }
+	void SetLens(float fovY, float aspect, float zn, float zf);
 
-	float& GetPitch() { return(m_fPitch); }
+	void LookAt(FXMVECTOR pos, FXMVECTOR target, FXMVECTOR worldUp);
+	void LookAt(const XMFLOAT3& pos, const XMFLOAT3& target, const XMFLOAT3& up);
 
-	float& GetRoll() { return(m_fRoll); }
+	XMMATRIX GetView()const;
+	XMMATRIX GetProj()const;
 
-	float& GetYaw() { return(m_fYaw); }
+	XMFLOAT4X4 GetView4x4f()const;
+	XMFLOAT4X4 GetProj4x4f()const;
 
+	void Strafe(float d);
+	void Walk(float d);
 
+	void Pitch(float angle);
+	void RotateY(float angle);
 
-	// void SetOffset(XMFLOAT3 xmf3Offset) { m_xmf3Offset = xmf3Offset; } 
+	void UpdateViewMatrix();
 
-	void SetOffset(XMFLOAT3 xmf3Offset)
+private:
+	XMFLOAT3 mPosition = { 0.0f, 0.0f, 0.0f };
+	XMFLOAT3 mRight = { 1.0f, 0.0f, 0.0f };
+	XMFLOAT3 mUp = { 0.0f, 1.0f, 0.0f };
+	XMFLOAT3 mLook = { 0.0f, 0.0f, 1.0f };
 
-	{
+	float mNearZ = 0.0f;
+	float mFarZ = 0.0f;
+	float mAspect = 0.0f;
+	float mFovY = 0.0f;
+	float mNearWindowHeight = 0.0f;
+	float mFarWindowHeight = 0.0f;
 
-		m_xmf3Offset = xmf3Offset; m_xmf3Position.x += xmf3Offset.x; m_xmf3Position.y += xmf3Offset.y; m_xmf3Position.z += xmf3Offset.z;
+	bool mViewDirty = true;
 
-	}
-
-	XMFLOAT3& GetOffset() { return(m_xmf3Offset); }
-
-	void SetTimeLag(float fTimeLag) { m_fTimeLag = fTimeLag; }
-
-	float GetTimeLag() { return(m_fTimeLag); }
-
-	XMFLOAT4X4 GetViewMatrix() { return(m_xmf4x4View); }
-
-	XMFLOAT4X4 GetProjectionMatrix() { return(m_xmf4x4Projection); }
-
-	D3D12_VIEWPORT GetViewport() { return(m_d3dViewport); }
-
-	D3D12_RECT GetScissorRect() { return(m_d3dScissorRect); }
-
-	//카메라를 xmf3Shift 만큼 이동한다. 
-
-	virtual void Move(const XMFLOAT3& xmf3Shift)
-
-	{
-
-		m_xmf3Position.x += xmf3Shift.x;
-
-		m_xmf3Position.y += xmf3Shift.y;
-
-		m_xmf3Position.z += xmf3Shift.z;
-
-	}
-
-	//카메라를 x-축, y-축, z-축으로 회전하는 가상함수이다. 
-
-	virtual void Rotate(float fPitch = 0.0f, float fYaw = 0.0f, float fRoll = 0.0f) { }
-	virtual void Update(XMFLOAT3& xmf3LookAt, float fTimeElapsed) { }
-	virtual void SetLookAt(XMFLOAT3& xmf3LookAt) { }
-
+	XMFLOAT4X4 mView = MathHelper::Identity4x4();
+	XMFLOAT4X4 mProj = MathHelper::Identity4x4();
 };
 
-
-
-class SpaceShipCamera : public Camera
-
-{
-
-public:
-
-	SpaceShipCamera(Camera *pCamera);
-
-	virtual ~SpaceShipCamera() { }
-
-	virtual void Rotate(float fPitch = 0.0f, float fYaw = 0.0f, float fRoll = 0.0f);
-
-};
-
-class FirstPersonCamera : public Camera
-
-{
-
-public:
-
-	FirstPersonCamera(Camera *pCamera);
-
-	virtual ~FirstPersonCamera() { }
-
-	virtual void Rotate(float fPitch = 0.0f, float fYaw = 0.0f, float fRoll = 0.0f);
-
-};
-
-class ThirdPersonCamera : public Camera
-
-{
-
-public:
-
-	ThirdPersonCamera(Camera *pCamera);
-
-	virtual ~ThirdPersonCamera() { }
-
-	virtual void Update(XMFLOAT3& xmf3LookAt, float fTimeElapsed);
-
-	virtual void SetLookAt(XMFLOAT3& vLookAt);
-
-};
+#endif // CAMERA_H
--- Client/Framework Project/Framework Project/Utility.h
+++ Client/Framework Project/Framework Project/Utility.h
@@ -21,315 +21,15 @@
 #include <sstream>
 #include <cassert>
 #include "d3dx12.h"
+
+
+#define FRAME_BUFFER_WIDTH 640
+#define FRAME_BUFFER_HEIGHT 480
 #define EPSILON 1.0e-8f
+
+using namespace DirectX;
 extern const int gNumFrameResources;
 inline bool IsZero(float fValue) { return((fabsf(fValue) < EPSILON)); }
-
-// TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
-
-
-
-//3차원 벡터의 연산
-
-namespace Vector3 {
-	
-	XMFLOAT3 XMVectorToFloat3(XMVECTOR& xmvVector)
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		XMStoreFloat3(&xmf3Result, xmvVector);
-
-		return(xmf3Result);
-
-	}
-
-	inline XMFLOAT3 ScalarProduct(XMFLOAT3& xmf3Vector, float fScalar, bool bNormalize = true)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		if (bNormalize)
-
-			XMStoreFloat3(&xmf3Result, XMVector3Normalize(XMLoadFloat3(&xmf3Vector)) * fScalar);
-
-		else
-
-			XMStoreFloat3(&xmf3Result, XMLoadFloat3(&xmf3Vector) * fScalar); return(xmf3Result);
-
-	}
-
-	inline XMFLOAT3 Add(const XMFLOAT3& xmf3Vector1, const XMFLOAT3& xmf3Vector2)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		XMStoreFloat3(&xmf3Result, XMLoadFloat3(&xmf3Vector1) + XMLoadFloat3(&xmf3Vector2));
-
-		return(xmf3Result);
-
-	}
-
-	inline XMFLOAT3 Add(XMFLOAT3& xmf3Vector1, XMFLOAT3& xmf3Vector2, float fScalar)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		XMStoreFloat3(&xmf3Result, XMLoadFloat3(&xmf3Vector1) + (XMLoadFloat3(&xmf3Vector2) * fScalar));
-
-		return(xmf3Result);
-
-	}
-
-	inline XMFLOAT3 Subtract(XMFLOAT3& xmf3Vector1, XMFLOAT3& xmf3Vector2)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		XMStoreFloat3(&xmf3Result, XMLoadFloat3(&xmf3Vector1) - XMLoadFloat3(&xmf3Vector2));
-
-		return(xmf3Result);
-
-	}
-
-	inline float DotProduct(XMFLOAT3& xmf3Vector1, XMFLOAT3& xmf3Vector2)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		XMStoreFloat3(&xmf3Result, XMVector3Dot(XMLoadFloat3(&xmf3Vector1), XMLoadFloat3(&xmf3Vector2)));
-
-		return(xmf3Result.x);
-
-	}
-
-	inline XMFLOAT3 CrossProduct(XMFLOAT3& xmf3Vector1, XMFLOAT3& xmf3Vector2, bool bNormalize = true)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		if (bNormalize)
-
-			XMStoreFloat3(&xmf3Result, XMVector3Normalize(XMVector3Cross(XMLoadFloat3(&xmf3Vector1), XMLoadFloat3(&xmf3Vector2))));
-
-		else
-
-			XMStoreFloat3(&xmf3Result, XMVector3Cross(XMLoadFloat3(&xmf3Vector1), XMLoadFloat3(&xmf3Vector2)));
-
-		return(xmf3Result);
-
-	}
-
-	inline XMFLOAT3 Normalize(XMFLOAT3& xmf3Vector)
-
-	{
-
-		XMFLOAT3 m_xmf3Normal;
-
-		XMStoreFloat3(&m_xmf3Normal, XMVector3Normalize(XMLoadFloat3(&xmf3Vector)));
-
-		return(m_xmf3Normal);
-
-	}
-
-	inline float Length(XMFLOAT3& xmf3Vector)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		XMStoreFloat3(&xmf3Result, XMVector3Length(XMLoadFloat3(&xmf3Vector)));
-
-		return(xmf3Result.x);
-
-	}
-
-	inline float Angle(XMVECTOR& xmvVector1, XMVECTOR& xmvVector2)
-
-	{
-
-		XMVECTOR xmvAngle = XMVector3AngleBetweenNormals(xmvVector1, xmvVector2);
-
-		return(XMConvertToDegrees(acosf(XMVectorGetX(xmvAngle))));
-
-	}
-
-	inline float Angle(XMFLOAT3& xmf3Vector1, XMFLOAT3& xmf3Vector2)
-
-	{
-
-		return(Angle(XMLoadFloat3(&xmf3Vector1), XMLoadFloat3(&xmf3Vector2)));
-
-	}
-
-	inline XMFLOAT3 TransformNormal(XMFLOAT3& xmf3Vector, XMMATRIX& xmmtxTransform)
-
-	{
-
-		XMFLOAT3 xmf3Result;
-
-		XMStoreFloat3(&xmf3Result, XMVector3TransformNormal(XMLoadFloat3(&xmf3Vector), xmmtxTransform));
-
-		return(xmf3Result);
-
-	}
-
-	inline XMFLOAT3 TransformCoord(XMFLOAT3& xmf3Vector, XMMATRIX& xmmtxTransform)
-
-	{
-
-		XMFLOAT3 xmf3Result; XMStoreFloat3(&xmf3Result, XMVector3TransformCoord(XMLoadFloat3(&xmf3Vector), xmmtxTransform));
-
-		return(xmf3Result);
-
-	}
-
-	inline XMFLOAT3 TransformCoord(XMFLOAT3& xmf3Vector, XMFLOAT4X4& xmmtx4x4Matrix)
-
-	{
-
-		return(TransformCoord(xmf3Vector, XMLoadFloat4x4(&xmmtx4x4Matrix)));
-
-	}
-
-}
-
-//4차원 벡터의 연산 
-
-namespace Vector4 {
-
-	inline XMFLOAT4 Multiply(float fScalar, XMFLOAT4& xmf4Vector)
-
-	{
-
-		XMFLOAT4 xmf4Result;
-
-		XMStoreFloat4(&xmf4Result, fScalar * XMLoadFloat4(&xmf4Vector));
-
-		return(xmf4Result);
-
-	}
-
-	inline XMFLOAT4 Add(XMFLOAT4& xmf4Vector1, XMFLOAT4& xmf4Vector2)
-
-	{
-
-		XMFLOAT4 xmf4Result; XMStoreFloat4(&xmf4Result, XMLoadFloat4(&xmf4Vector1) + XMLoadFloat4(&xmf4Vector2));
-
-		return(xmf4Result);
-
-	}
-
-}
-
-//행렬의 연산 
-
-namespace Matrix4x4
-
-{
-
-	inline XMFLOAT4X4 Identity() {
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, XMMatrixIdentity());
-
-		return(xmmtx4x4Result);
-
-	}
-
-	inline XMFLOAT4X4 Multiply(XMFLOAT4X4& xmmtx4x4Matrix1, XMFLOAT4X4& xmmtx4x4Matrix2)
-
-	{
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, XMLoadFloat4x4(&xmmtx4x4Matrix1) * XMLoadFloat4x4(&xmmtx4x4Matrix2));
-
-		return(xmmtx4x4Result);
-
-	}
-
-	inline XMFLOAT4X4 Multiply(XMFLOAT4X4& xmmtx4x4Matrix1, XMMATRIX& xmmtxMatrix2)
-
-	{
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, XMLoadFloat4x4(&xmmtx4x4Matrix1) * xmmtxMatrix2);
-
-		return(xmmtx4x4Result);
-
-	}
-
-	inline XMFLOAT4X4 Multiply(XMMATRIX& xmmtxMatrix1, XMFLOAT4X4& xmmtx4x4Matrix2)
-
-	{
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, xmmtxMatrix1 * XMLoadFloat4x4(&xmmtx4x4Matrix2));
-
-		return(xmmtx4x4Result);
-
-	}
-
-	inline XMFLOAT4X4 Inverse(XMFLOAT4X4& xmmtx4x4Matrix)
-
-	{
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, XMMatrixInverse(NULL, XMLoadFloat4x4(&xmmtx4x4Matrix)));
-
-		return(xmmtx4x4Result);
-
-	}
-
-	inline XMFLOAT4X4 Transpose(XMFLOAT4X4& xmmtx4x4Matrix)
-
-	{
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, XMMatrixTranspose(XMLoadFloat4x4(&xmmtx4x4Matrix)));
-
-		return(xmmtx4x4Result);
-
-	}
-
-	inline XMFLOAT4X4 PerspectiveFovLH(float FovAngleY, float AspectRatio, float NearZ, float FarZ)
-
-	{
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, XMMatrixPerspectiveFovLH(FovAngleY, AspectRatio, NearZ, FarZ));
-
-		return(xmmtx4x4Result);
-
-	}
-
-	inline XMFLOAT4X4 LookAtLH(XMFLOAT3& xmf3EyePosition, XMFLOAT3& xmf3LookAtPosition, XMFLOAT3& xmf3UpDirection)
-
-	{
-
-		XMFLOAT4X4 xmmtx4x4Result;
-
-		XMStoreFloat4x4(&xmmtx4x4Result, XMMatrixLookAtLH(XMLoadFloat3(&xmf3EyePosition), XMLoadFloat3(&xmf3LookAtPosition), XMLoadFloat3(&xmf3UpDirection)));
-
-		return(xmmtx4x4Result);
-
-	}
-
-}
-
 
 inline void d3dSetDebugName(IDXGIObject* obj, const char* name)
 {
@@ -359,7 +59,6 @@ inline std::wstring AnsiToWString(const std::string& str)
 	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
 	return std::wstring(buffer);
 }
-
 
 class Utility
 {
@@ -409,7 +108,7 @@ struct SubmeshGeometry
 	UINT StartIndexLocation = 0;
 	INT BaseVertexLocation = 0;
 
-	DirectX::BoundingBox Bounds;
+	BoundingBox Bounds;
 };
 
 struct MeshGeometry
@@ -461,11 +160,11 @@ struct MeshGeometry
 
 struct Light
 {
-	DirectX::XMFLOAT3 Strength = { 0.5f, 0.5f, 0.5f };
+	XMFLOAT3 Strength = { 0.5f, 0.5f, 0.5f };
 	float FalloffStart = 1.0f;                          
-	DirectX::XMFLOAT3 Direction = { 0.0f, -1.0f, 0.0f };
+	XMFLOAT3 Direction = { 0.0f, -1.0f, 0.0f };
 	float FalloffEnd = 10.0f;                           
-	DirectX::XMFLOAT3 Position = { 0.0f, 0.0f, 0.0f };  
+	XMFLOAT3 Position = { 0.0f, 0.0f, 0.0f };  
 	float SpotPower = 64.0f;                            
 };
 
@@ -473,12 +172,12 @@ struct Light
 
 //struct MaterialConstants
 //{
-//	DirectX::XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
-//	DirectX::XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
+//	XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
+//	XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
 //	float Roughness = 0.25f;
 //
 //	// Used in texture mapping.
-//	DirectX::XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
+//	XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
 //};
 //
 //// Simple struct to represent a material for our demos.  A production 3D engine
@@ -504,10 +203,10 @@ struct Light
 //	int NumFramesDirty = gNumFrameResources;
 //
 //	// Material constant buffer data used for shading.
-//	DirectX::XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
-//	DirectX::XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
+//	XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
+//	XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
 //	float Roughness = .25f;
-//	DirectX::XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
+//	XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
 //};
 //
 //struct Texture
