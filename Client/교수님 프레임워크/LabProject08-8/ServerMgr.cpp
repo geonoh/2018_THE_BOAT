6b30f59157ae6b0db9c5818573cccc210e9aaf0b
Oh Geon <ohhh123@naver.com>
2018-06-01 14:59:41 +0900

    [클라이언트] 테스트환경 구축

--- Client/교수님 프레임워크/LabProject08-8/ServerMgr.cpp
+++ Client/교수님 프레임워크/LabProject08-8/ServerMgr.cpp
@@ -34,8 +34,8 @@ void ServerMgr::Initialize(HWND& hwnd) {
 	ServerAddr.sin_family = AF_INET;
 	ServerAddr.sin_port = htons(SERVER_PORT);
 	// ¾ÆÀÌÇÇ
-	//ServerAddr.sin_addr.s_addr = inet_addr(server_ip.c_str());
-	ServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ServerAddr.sin_addr.s_addr = inet_addr(server_ip.c_str());
+	//ServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	//ServerAddr.sin_addr.s_addr = inet_addr("114.204.69.71");
 
 
