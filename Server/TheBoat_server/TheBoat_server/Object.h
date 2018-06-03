351d1c3486707be15cba281ef0a03fc39db4eb33
Oh Geon <ohhh123@naver.com>
2018-06-03 23:43:33 +0900

    [서버] 빌딩 충돌체크 완료

--- Server/TheBoat_server/TheBoat_server/Object.h
+++ Server/TheBoat_server/TheBoat_server/Object.h
@@ -4,11 +4,11 @@ class Object
 {
 protected:
 	XMFLOAT3 position;
-	BoundingOrientedBox bounding_box;
 	XMFLOAT3 obb_extents;	// Object OBB Size
 
 	void SetOBB(XMFLOAT3 xmCenter, XMFLOAT3 xmExtents, XMFLOAT4 xmOrientation);
 public:
+	BoundingOrientedBox bounding_box;
 	void SetPosition(XMFLOAT3& input_pos, XMFLOAT3& extents);
 
 	virtual XMFLOAT3 GetPosition();
